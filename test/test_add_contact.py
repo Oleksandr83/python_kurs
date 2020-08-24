51ea36f40a9d8a97a4300ad464b181ac2365b911
Oleksandr83 <kamika@bigmir.net>
2020-08-24 16:25:53 +0200

    очищено лишнее

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#import unittest
+
 import pytest
 from fixture.application import Application
 from model.contact import Contact
@@ -10,13 +10,7 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-'''class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-        #self.wd = webdriver.Firefox()
-        #self.wd.implicitly_wait(30)'''
 
-    
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(contact_firstname="Alex", contact_lastname="Myniuk", contact_adress="Sweden"))
@@ -30,62 +24,4 @@ def test_add_empty_contact(app):
     app.session.logout()
 
 
-    #def logout(self):
-    #    wd = self.wd
-    #    wd.find_element_by_link_text("Logout").click()
 
-    
-
-    '''def return_to_home_page(self:
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        self.open_contact_page()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.contact_firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.contact_lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.contact_adress)
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def open_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()'''
-
-    '''def login(self,username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")'''
-
-    #def is_element_present(self, how, what):
-     #   try: self.wd.find_element(by=how, value=what)
-     #  except NoSuchElementException as e: return False
-      #  return True
-    
-    #def is_alert_present(self):
-     #   try: self.wd.switch_to_alert()
-     #   except NoAlertPresentException as e: return False
-     #   return True
-    
-    #def tearDown(self):
-    #    self.app.destroy()
-
-#if __name__ == "__main__":
-#    unittest.main()
