e07568e42ef6867ece3abf0a32eb3b5e71db5040
Oleksandr83 <kamika@bigmir.net>
2020-08-27 16:44:44 +0200

    Сбой теста для редактирования контакта (строка адреса)

--- fixture/contact.py
+++ fixture/contact.py
@@ -24,6 +24,19 @@ class ContactHelper:
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.return_to_home_page()
 
+    def edit_first_contact(self):
+        wd = self.app.wd
+        self.open_contact_page()
+        # select edition for the first contact
+        wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
+        # edit contact info
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Edited")
+        # submit contact updating
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
