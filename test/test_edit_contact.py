931707636907b21597319d69ebfe40e870b92398
Oleksandr83 <kamika@bigmir.net>
2020-09-09 15:36:48 +0200

    добавлены тесты на проверку оставшихся параметров при редактировании

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -17,14 +17,32 @@ def test_edit_firstname_contact(app):
     assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
     #app.session.logout()
 
-'''def test_edit_lastname_contact(app):
+def test_edit_lastname_contact(app):
     #app.session.login(username="admin", password="secret")
     app.contact.check_contact_existence()
-    app.contact.edit_first_contact(Contact(contact_lastname="New lastname"))
+    old_contacts_list = app.contact.get_contact_list()
+    contact = Contact(contact_lastname="New lastname")
+    contact.id = old_contacts_list[0].id  # запоминание идентификатора
+    contact.contact_firstname = old_contacts_list[0].contact_firstname
+    contact.contact_address = old_contacts_list[0].contact_address
+    app.contact.edit_first_contact(contact)
+    new_contacts_list = app.contact.get_contact_list()
+    assert len(old_contacts_list) == len(new_contacts_list)
+    old_contacts_list[0] = contact
+    assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
     #app.session.logout()
 
 def test_edit_adress_contact(app):
     #app.session.login(username="admin", password="secret")
     app.contact.check_contact_existence()
-    app.contact.edit_first_contact(Contact(contact_address="New address"))
-    #app.session.logout()'''
+    old_contacts_list = app.contact.get_contact_list()
+    contact = Contact(contact_address="New address")
+    contact.id = old_contacts_list[0].id  # запоминание идентификатора
+    contact.contact_firstname = old_contacts_list[0].contact_firstname
+    contact.contact_lastname = old_contacts_list[0].contact_lastname
+    app.contact.edit_first_contact(contact)
+    new_contacts_list = app.contact.get_contact_list()
+    assert len(old_contacts_list) == len(new_contacts_list)
+    old_contacts_list[0] = contact
+    assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
+    #app.session.logout()
