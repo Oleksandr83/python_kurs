1f4d857850874edfcc4b4317db2e93131c08ddbb
Oleksandr83 <kamika@bigmir.net>
2020-09-10 12:21:11 +0200

    реализованно кеширование и хешировние для тестов груп

--- fixture/group.py
+++ fixture/group.py
@@ -20,6 +20,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def edit_first_group(self, new_group_data):
         wd = self.app.wd
@@ -32,6 +33,7 @@ class GroupHelper:
         # submit group updating
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -54,6 +56,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -74,12 +77,15 @@ class GroupHelper:
         if self.count() == 0:
             self.create(Group(name="test"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text =element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text =element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache) #будем возвращать копию кеша, потому как сам кем может быть где-то испорчен
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -17,12 +17,12 @@ def test_add_group(app):
     group = Group(name="new group", header="some info", footer="other")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count() #len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     #app.session.logout()
 
-def test_add_empty_group(app):
+'''def test_add_empty_group(app):
     #app.session.login(username="admin", password="secret")
     old_groups = app.group.get_group_list()
     group = Group(name="", header="", footer="")
@@ -31,5 +31,5 @@ def test_add_empty_group(app):
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    #app.session.logout()
+    #app.session.logout()'''
 
