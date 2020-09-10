9d4d9caffe42e0f051c192ed1ecf087236372757
Oleksandr83 <kamika@bigmir.net>
2020-09-10 13:20:51 +0200

    изменение теста на изменение первой группы, теперь изменение происходит случайной произвольной группы из списка

--- fixture/group.py
+++ fixture/group.py
@@ -22,10 +22,13 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def edit_first_group(self, new_group_data):
+    def edit_first_group(self):
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_group_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open edit form
         wd.find_element_by_name("edit").click()
         # make edition
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,18 @@
 from model.group import Group
+from random import randrange
 
 
 def test_edit_group_name(app):
     #app.session.login( username="admin", password="secret")
     app.group.check_group_existence()
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="New group")
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(index, group) #app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     #app.session.logout()
 
