f62da352caadc2ef9e0ecc009b20030818f2b82c
Oleksandr83 <kamika@bigmir.net>
2020-09-23 12:50:53 +0200

    вынесение данных для создания контактов  в отдельный файл

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,32 +4,34 @@ import pytest
 from fixture.application import Application
 from model.contact import Contact
 import pytest
+#from data.add_contact import testdata # генерируемые данные
+from data.add_contact import constant as testdata # использование фиксированных данных
 import random
 import string
 
-'''@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture'''
+#@pytest.fixture
+#def app(request):
+#    fixture = Application()
+#    request.addfinalizer(fixture.destroy)
+ #   return fixture
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*5
-    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
-
-def random_numbers(prefix, maxlen):
-    #numbers = string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(string.digits) for i in range (random.randrange(maxlen))])
-
-def random_email(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + "@"*10 + "."
-    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
-
-testdata =[Contact(contact_firstname="", contact_lastname="", contact_address="",contact_homephone="", contact_email="")] + [
-        Contact(contact_firstname=random_string("firstname", 15), contact_lastname=random_string("lastname", 20), contact_address=random_string("address", 10), contact_homephone=random_numbers("tel", 12), contact_email=random_email("email", 30))
-        for i in range(2)
-]
+#def random_string(prefix, maxlen):
+#    symbols = string.ascii_letters + string.digits + " "*5
+#    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+#
+#def random_numbers(prefix, maxlen):
+#    #numbers = string.digits + string.punctuation + " "*10
+#    return prefix + "".join([random.choice(string.digits) for i in range (random.randrange(maxlen))])
+#
+#def random_email(prefix, maxlen):
+#    symbols = string.ascii_letters + string.digits + "@"*10 + "."
+#    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+#
+#testdata =[Contact(contact_firstname="", contact_lastname="", contact_address="",contact_homephone="", contact_email="")] + [
+#        Contact(contact_firstname=random_string("firstname", 15), contact_lastname=random_string("lastname", 20), contact_address=random_string("address", 10), contact_homephone=random_numbers("tel", 12), contact_email=random_email("email", 30))
+#        for i in range(2)
+#]
 
 #testdata = [
 #        Contact(contact_firstname=name, contact_lastname=lastname, contact_address=address, contact_homephone=homephone, contact_email=contact_email)
@@ -51,17 +53,6 @@ def test_add_contact(app, contact):
     old_contacts_list.append(contact)
     assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
 
-'''def test_add_empty_contact(app):
-    #app.session.login(username="admin", password="secret")
-    old_contacts_list = app.contact.get_contact_list()
-    contact = Contact(contact_firstname="", contact_lastname="", contact_address="")
-    app.contact.create(contact)
-    new_contacts_list = app.contact.get_contact_list()
-    assert len(old_contacts_list) + 1 == len(new_contacts_list)
-    old_contacts_list.append(contact)
-    assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contacts_list, key=Contact.id_or_max)
-    #app.contact.return_to_home_page()
-    #app.session.logout()'''
 
 
 
