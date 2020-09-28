0af7bb015c657bb728ff43582bea2445005a06d9
Oleksandr83 <kamika@bigmir.net>
2020-09-28 13:28:58 +0200

    сравнение спаска взятого из базы дынных и из UI для групп

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -6,3 +6,6 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list = db.ger_group_list()
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
