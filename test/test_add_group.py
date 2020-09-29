da30385ca4afd00ef09832b0260a235fdc50dab9
Oleksandr83 <kamika@bigmir.net>
2020-09-29 12:17:41 +0200

    ускорена проверка доьавления контактов с помощью извлечения информации из базы данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,13 @@
 from model.group import Group
 
 # фикстура указывающая на источник данных
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     #group = Group(name="new group", header="some info", footer="other")
     app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == app.group.count() #len(new_groups)
+    #assert len(old_groups) + 1 == app.group.count() #len(new_groups)
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     #app.session.logout()
