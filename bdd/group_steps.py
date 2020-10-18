7e66bebdf426edbd6a8544bf015ce450c3b54c6b
Oleksandr83 <kamika@bigmir.net>
2020-10-18 14:18:00 +0200

    тест и сценарий написанный на псвдоестественном языке для удаления групп

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -2,8 +2,9 @@
 
 from pytest_bdd import given, when, then
 from model.group import Group
+import random
 
-@given('a group list',target_fixture="group_list") #добавлено помимо лекции target_fixture="group_list", из переписки
+@given('a group list', target_fixture="group_list") #добавлено помимо лекции target_fixture="group_list", из переписки
 def group_list(db):
     return db.get_group_list()
 
@@ -21,6 +22,33 @@ def verify_group_edit(db, group_list, new_group):
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+@given('a non-empty group list', target_fixture="non_empty_group_list")
+def non_empty_group_list(db, app):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="some group"))
+    return db.get_group_list()
+
+@given('a random group from the list', target_fixture="random_group")
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+@when('I delete the group from the list')
+def delete_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+@then('the new group list is equal to the old group list without the deleted group')
+def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+    # app.session.logout()
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
 
 
     '''интересно... судя по всему, в новых версиях pytest-bdd больше не создаётся автоматически фикстура с тем же именем, что и у функции, вместо неё создаётся фикстура со сложным именем "pytestbdd_given_a group with <name>, <header> and <footer>"
